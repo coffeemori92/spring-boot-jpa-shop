5588af930c7350b9801973311555c72493d9ab04
coffeemori92 <coffeemori92@naver.com>
2020-12-21 20:47:58 +0900

    도메인 분석 설계

---
+++ src/main/java/jpabook/jpashop/domains/OrderedItem.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domains;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import jpabook.jpashop.domains.Item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class OrderedItem {
+
+  @Id @GeneratedValue
+  @Column(name = "order_item_id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "item_id")
+  private Item item;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "order_id")
+  private Order order;
+
+  private int orderPrice; // 주문가격
+  private int count; // 주문수량
+
+}
